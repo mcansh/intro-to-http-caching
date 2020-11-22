b60f714f3cef36cc4ca01cc9b47aba9d2b43f420
Logan McAnsh <logan@mcan.sh>
2020-11-22 12:34:01 -0500

    chore: update page title
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- api/page-3.ts
+++ api/page-3.ts
@@ -2,7 +2,7 @@ import type { NowApiHandler } from "@vercel/node";
 import { createETag, createPage } from "./_utils";
 
 const handler: NowApiHandler = (req, res) => {
-  const html = createPage("Page 2");
+  const html = createPage("Page 3 (SWR)");
   const etag = createETag(html);
 
   if (etag === req.headers["if-none-match"]) {
